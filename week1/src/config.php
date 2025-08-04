68fc0b9f4e803bb61702ea9dd1fafb967077c57d
wiimdy <wiimdy@macbook.pro>
2025-08-05 00:00:22 +0900

    fix(config): Change strtok func

--- week1/src/config.php
+++ week1/src/config.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if (!isset($_SESSION['id']) && strtok($_SERVER['REQUEST_URI']) !='/login.php'){
+    if (!isset($_SESSION['id']) && strtok($_SERVER['REQUEST_URI'], '/') !='login.php'){
         header("Location: /login.php");
         exit();
     }
