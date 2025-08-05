a6905cf3e44e566b6833b2df9e169d41f77d5aef
wiimdy <wiimdy@macbook.pro>
2025-08-05 20:34:44 +0900

    feat(board): Add delete

---
+++ week1/src/board/board_delete.php
@@ -0,0 +1,47 @@
+<?php 
+session_start();
+if(!isset($_SESSION['username'])) {
+    echo("<script>alert('No Delete Permission')</script>");
+    echo("<script>location.href='/index.php';</script>");
+    exit();
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] != 'GET') {
+    header("Location: index.php");
+    exit();
+}
+if (!isset($_GET['id'])) {
+    echo("<script>alert('Wrong Access')</script>");
+    echo("<script>location.href='/index.php';</script>");
+    exit();
+}
+
+$username = $_SESSION['username'];
+$conn = mysqli_connect(getenv("DB_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+if ($conn->connect_error) {
+    echo "db error";
+    exit();
+}
+
+$id = $_GET['id'];
+$sql = "SELECT * FROM boards WHERE id = ?";
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+$result = mysqli_stmt_get_result($stmt);
+$row_count = mysqli_num_rows($result);
+if ($row_count != 1){
+    echo("<script>alert('Wrong ID')</script>");
+    echo("<script>location.href='/index.php';</script>");
+    exit();
+}
+
+$sql = "DELETE FROM boards WHERE id = ?";
+$stmt = mysqli_prepare($conn, $sql);
+mysqli_stmt_bind_param($stmt, "i", $id);
+mysqli_stmt_execute($stmt);
+echo("<script>alert('Success delete')</script>");
+echo("<script>location.href='/index.php';</script>");
+// $result = mysqli_stmt_get_result($stmt);
+?>
