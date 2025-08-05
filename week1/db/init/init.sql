a5e76cb2ff6b0b7c32f08760cd00f008b77c1824
wiimdy <wiimdy@macbook.pro>
2025-08-05 21:03:30 +0900

    refactor(db): change writer

--- week1/db/init/init.sql
+++ week1/db/init/init.sql
@@ -12,9 +12,9 @@ INSERT INTO users (username, password) VALUES ('admin', SHA2('adminn',256)), ('g
 
 CREATE TABLE boards (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
+  writer VARCHAR(50) NOT NULL,
   title CHAR(64) NOT NULL,
   content text NOT NULL
 ) ENGINE=InnoDB;
 
-INSERT INTO boards (username, title, content) VALUES ('admin', 'Notice', "Hello everyone!!");
+INSERT INTO boards (writer, title, content) VALUES ('admin', 'Notice', "Hello everyone!!");
