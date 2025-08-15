766d59dbab541cca8880573e5c0af8f895591f91
wiimdy <wiimdy@macbook.pro>
2025-08-15 17:25:21 +0900

    feat(config): rate Limit config

---
+++ week2/backend/src/config/upstash.js
@@ -0,0 +1,12 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  ratelimit: Ratelimit.slidingWindow(10, "10 s"),
+});
+
+export default ratelimit;
