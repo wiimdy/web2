0b3fa231dc745e901040f2d432b785b6c5cd04d7
wiimdy <wiimdy@macbook.pro>
2025-08-18 23:47:06 +0900

    feat(front): Add CORS, axiosInstance

--- week2/frontend/src/pages/HomePage.jsx
+++ week2/frontend/src/pages/HomePage.jsx
@@ -2,20 +2,33 @@ import React, { useState } from 'react'
 import NavBar from '../components/NavBar'
 import RateLimitedUI from '../components/RateLimitedUI'
 import { useEffect } from 'react'
-import axios from "axios"
+import api from '../lib/anxios'
+import {toast} from "react-hot-toast"
 
 const HomePage = () => {
     const [israteLimted, setIsRateLimited] = useState(false)
-    const [isNote, setisNote] = useState(true)
+    const [isNote, setisNote] = useState([])
+    const [loading, setLoading] = useState(true)
+
 
     useEffect(() => {
         const fetchNote = async () => {
             try {
-                const res = await axios.get("http://localhost:8777/api/note")
-                const data = await res.json()
-                console.log(data )
+                const res = await api.get("/note", { withCredentials: true })
+                console.log(res.data)
+                setisNote(res.data.length > 0)
+                setIsRateLimited(false)
             } catch (error) {
-                
+                console.error("에러 발생:", error)
+                if (error.response.status === 429) {
+                    setIsRateLimited(true)
+                }
+                else {
+                    toast.error("Failed to load notes")
+                }
+            }
+            finally {
+                setLoading(false)
             }
         }
         fetchNote()
